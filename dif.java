8e8102fe3fe2ce6e174a53912331dd5b3213b2b8
akash <akashrathore892378@gmail.com>
2023-05-23 19:52:07 +0530

    added implementation in dif.java

--- dif.java
+++ dif.java
@@ -1,6 +1,32 @@
-public class dif {
-    public static void main(String[] args) {
-        int b =45;
-        System.out.println(b);
-    }
+import java .util.*;
+
+public class dif
+{
+	void func()
+	{
+		Set st = new HashSet();
+		
+		st.add("coke ");
+		st.add("momes ");
+		st.add("chicken");
+		st.add(100 );
+		st.add(6.7);
+		
+		if(st.isEmpty())
+		{
+			System.out.println("blank");
+		}
+		else 
+		{
+
+		System.out.println(st);
+		System.out.println("no.of items :"+st.size());
+		}
+	
+	}
+	public static void main(String args[])
+	{
+		dif obj =new dif();
+		obj.func();
+	}
 }
