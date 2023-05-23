76d0a9b27a56f6c67690679863df60c16a304a56
Akash <akash.rathour_cs20@gla.ac.in>
2023-05-23 15:00:42 +0530

    initial commit

---
+++ git.java
@@ -0,0 +1,11 @@
+/**
+ * git
+ */
+public class git {
+    public static void main(String[] args) {
+        int a = 3 ;
+        int b = 4 ;
+        int sum  = a + b ;
+        System.out.print(sum+"modified");
+    }
+}
